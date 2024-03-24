96abd14e86c489e454cd62299817715ebad4e816
Atharva Srivastava <atharva201004@gmail.com>
2024-03-24 15:20:40 +0530

    24/3/24 15:22 Added 1 New C Problem - HTMLparser.c

---
+++ C Problems/HtmlH1TagParser.c
@@ -0,0 +1,35 @@
+// This program takes h1 tag as a string, and parses it to get and print the contents of the heading and remove preceeding or succeeding spaces
+#include<stdio.h>
+#include<string.h>
+void parseAndPrint(char arr[]){
+    for (int i = 0; i < 4; i++){
+        arr[i] = ' ';
+    }
+    char* ptr = strchr(arr,'<');
+
+    char newArr[100];
+    int j = 0;
+    int Condition = 1;
+    char* newPtr;
+    for (int i = 0; i < 100; i++){
+        if (arr[i]== ' ' && Condition==1)
+            continue;
+        Condition = 0;
+        if ( ((int)&arr[i]) == (int)ptr ){
+            newArr[j] = '\0';
+            break;
+        }
+        newArr[j] = arr[i];
+        ++j;
+    }
+
+    puts(newArr);
+}
+
+
+int main(int argc, char const *argv[]){
+    char h1[100];
+    fgets(h1, sizeof(h1), stdin);
+    parseAndPrint(h1);
+    return 0;
+}
