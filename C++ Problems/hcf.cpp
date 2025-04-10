48ba4a3b094c824c7be365008f03ce011f5e222a
Atharva Srivastava <atharva201004@gmail.com>
2025-04-10 15:50:39 +0530

    10/4/2025 15:50 justa little change

--- C++ Problems/hcf.cpp
+++ C++ Problems/hcf.cpp
@@ -1,37 +1,63 @@
+// #include<iostream>
+// #include<set>
+// #include<cmath>
+// #include<algorithm>
+
+// using namespace std;
+
+// set<int> listFactors(int x){
+//     set<int> factors;
+    
+//     for (int i = 1; i <= sqrt(x); i++){
+//         if(x%i==0){
+//             factors.insert(i);
+//             factors.insert(x/i);
+//         }
+//     }
+//     return factors;
+// }
+
+// int main(int argc, char const *argv[]){
+    
+//     int x, y;
+//     cin>>x>>y;
+
+//     set<int> xx = listFactors(x);
+//     set<int> yy = listFactors(y);
+
+//     int hcf = 0;
+//     for(auto var : xx){
+//         if(yy.find(var) != yy.end()){hcf = var;}
+//     }
+
+//     cout<<hcf<<endl;
+    
+    
+//     return 0;
+// }
+
+
+
+
+// =========================================================================================================================================
+
+
+// ALTERNATE METHOD
+
 #include<iostream>
-#include<set>
-#include<cmath>
-#include<algorithm>
 
 using namespace std;
 
-set<int> listFactors(int x){
-    set<int> factors;
-    
-    for (int i = 1; i <= sqrt(x); i++){
-        if(x%i==0){
-            factors.insert(i);
-            factors.insert(x/i);
-        }
-    }
-    return factors;
-}
-
 int main(int argc, char const *argv[]){
     
-    int x, y;
+    int x,y;
     cin>>x>>y;
-
-    set<int> xx = listFactors(x);
-    set<int> yy = listFactors(y);
-
-    int hcf = 0;
-    for(auto var : xx){
-        if(yy.find(var) != yy.end()){hcf = var;}
-    }
-
-    cout<<hcf<<endl;
     
+    int hcf;
+    for (int i = 1; i <= min(x,y); i++){
+        if(x%i == 0 && y%i == 0) hcf = i;
+    }
+    cout<<hcf<<endl;
     
     return 0;
 }
