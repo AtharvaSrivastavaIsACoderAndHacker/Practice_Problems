efe5732435de0fcd914d3728e43bcbda28372587
Atharva Srivastava <atharva201004@gmail.com>
2025-08-10 23:59:30 +0530

    10/8/2025 23:59 ++_(some solutions dsa)

---
+++ C++ Problems/RecursionBasicProblems - DSA/fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int f(int n){
+    if (n<=1){return n;}
+    return f(n-1) + f(n-2);
+}
+
+int main(int argc, char const *argv[]){
+    
+    int x; cin>>x;
+    cout<<f(x)<<endl;
+    
+    return 0;
+}
